bdb9a0cabdf12faa0963d22b6216b942470a9dc3
kaist-jisungpark <jisungpark@kaist.ac.kr>
2018-03-09 20:53:50 +0900

    similar to veldyne plugin

---
+++ micron_sonar_plugin/build/CMakeFiles/micron_sonar_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/micron_sonar_plugin.dir/micron_sonar_plugin.cc.o"
+  "libmicron_sonar_plugin.pdb"
+  "libmicron_sonar_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/micron_sonar_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
